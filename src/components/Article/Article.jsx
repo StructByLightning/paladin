2ac72de396030e5bc58acd821085a9b775a99db8
Raven Schultz <msravenschultz@gmail.com>
2020-08-13 16:36:57 -0700

    Add Guardian link to article cards

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -7,14 +7,14 @@ function Article({article}) {
   let date = new Date(article.webPublicationDate);
   let dateString = `${date.getMonth()}/${date.getFullYear()}`;
 
-  return (<div key={article.id} className="article">
+  return (<a href={article.webUrl} key={article.id} className="article">
     <span className="title">{article.webTitle}</span>
     <div className="bottom">
       <span className="section">{article.sectionName}</span>
       <span className="type">{article.type}</span>
       <span className="date">{dateString}</span>
     </div>
-  </div>
+  </a>
 
   );
 }
