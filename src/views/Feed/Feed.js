98b8620b94d92ca69ab838670304d1178bb8f322
Raven Schultz <msravenschultz@gmail.com>
2020-08-13 15:50:22 -0700

    basic article listing

--- src/views/Feed/Feed.js
+++ src/views/Feed/Feed.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import "./Feed.css";
 
 function Feed() {
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://content.guardianapis.com/search?api-key=462ffdc3-5cc3-4099-9a29-fa39124aa001")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+        setArticles(data.response.results);
+      }, (err) => {
+        setLoading(true);
+        setError(err);
+      });
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="feed">
+      {loading && <div className="loader">Loading...</div>}
+      {error && <div className="error">
+        <span>Error:</span>
+        <span>{error}</span>
+      </div>}
+      {articles?.length > 0 && <div className="articles">
+        {articles.map((article) => {
+          return <div key={article.id} className="article">{article.id}</div>;
+        })}
+      </div>}
+    </main>
   );
 }
 
