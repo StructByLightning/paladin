168dcfb981cfcca857d80595127640887af86fbe
Raven Schultz <msravenschultz@gmail.com>
2020-08-14 08:31:52 -0700

    Add ondelayedchange

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -4,12 +4,23 @@ import PropTypes from "prop-types";
 import "./TextInput.scss";
 
 function TextInput({
+  onDelayedChange,
   onChange,
   label,
 }) {
+  const [delayedChange, setDelayedChange] = useState(null);
+
+
   return (
     <label className="text-input">
-      <input type="text" onChange={onChange} placeholder=" "/>
+      <input type="text" onChange={(e) => {
+        let value = e.target.value;
+        onChange(value);
+        clearTimeout(delayedChange);
+        setDelayedChange(setTimeout(() => {
+          onDelayedChange(value);
+        }, 500));
+      }} placeholder=" "/>
       <span className="label">{label}</span>
     </label>
 
@@ -17,7 +28,13 @@ function TextInput({
 }
 
 TextInput.propTypes = {
+  onDelayedChange: PropTypes.func,
   onChange: PropTypes.func,
+};
+
+TextInput.defaultProps = {
+  onDelayedChange: () => {},
+  onChange: () => {},
 };
 
 export default TextInput;
