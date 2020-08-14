04d785e308572ccb5403fefc5c2397949f2ab6bc
Raven Schultz <msravenschultz@gmail.com>
2020-08-14 09:06:18 -0700

    refactor

--- src/views/Feed/Feed.js
+++ src/views/Feed/Feed.js
@@ -12,34 +12,39 @@ function Feed() {
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
+  const [searchHandle, setSearchHandle] = useState(null);
 
   useEffect(() => {
-    fetch(`https://content.guardianapis.com/search?api-key=462ffdc3-5cc3-4099-9a29-fa39124aa001&q=${search}&page=${page}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setLoading(false);
-        setArticles(data.response.results);
-      }, (err) => {
-        setLoading(true);
-        setError(err);
-        console.error();
-      });
+    clearTimeout(searchHandle);
+    setSearchHandle(setTimeout(() => {
+      fetch(`https://content.guardianapis.com/search?api-key=462ffdc3-5cc3-4099-9a29-fa39124aa001&q=${search}&page=${page}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setLoading(false);
+          setArticles(data.response.results);
+        }, (err) => {
+          setLoading(true);
+          setError(err);
+          console.error();
+        });
+    }, 500));
   }, [search, page]);
-
 
 
   return (
     <main className="feed">
-      <TextInput label="Search" onChange={() => {
+      <TextInput label="Search" onChange={(value) => {
         setLoading(true);
         setArticles([]);
-      }}
-      onDelayedChange={(value) => {
         setSearch(value);
+        setError("");
       }}/>
 
       <Paginator onChange={(value) => {
         setPage(value);
+        setLoading(true);
+        setArticles([]);
+        setError("");
       }} />
 
       {loading && !error && <div className="alert">Loading...</div>}
