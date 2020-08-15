d30036b467166dd81cad4f86aa361b71212a38ff
Raven Schultz <msravenschultz@gmail.com>
2020-08-15 10:57:23 -0700

    Add paginator buttons

--- src/components/Paginator/Paginator.jsx
+++ src/components/Paginator/Paginator.jsx
@@ -4,26 +4,27 @@ import PropTypes from "prop-types";
 import "./Paginator.scss";
 
 import TextInput from "components/TextInput/TextInput.jsx";
+import Button from "components/Buttons/Button.jsx";
+
 
 function Paginator({
   onChange,
   value,
+  onDecrement,
+  onIncrement,
 }) {
-  const [delayedChange, setDelayedChange] = useState(null);
-
-
   return (
     <div className="paginator">
-
+      <Button label="◄" onClick={onDecrement} disabled={value <= 1}/>
       <TextInput
         onChange={((val) => {
           onChange(val);
         })}
         value={value}
+        centered
       />
+      <Button label="►" onClick={onIncrement}/>
     </div>
-
-
   );
 }
 
@@ -31,10 +32,14 @@ function Paginator({
 Paginator.propTypes = {
   onChange: PropTypes.func,
   value: PropTypes.number.isRequired,
+  onDecrement: PropTypes.func,
+  onIncrement: PropTypes.func,
 };
 
 Paginator.defaultProps = {
   onChange: () => {},
+  onIncrement: () => {},
+  onDecrement: () => {},
 };
 
 export default Paginator;
